f79ac6a54ae1149c7ca326d9352cf0722e67d46b
DELL <DELL@DESKTOP-MMSF2EA>
2025-09-23 19:36:50 +0530

    commit

---
+++ corejavaprograms/src/day1/DatatypesDemo.java
@@ -0,0 +1,23 @@
+package day1;
+
+public class DatatypesDemo {
+
+	public static void main(String[] args) {
+		int value1 = 8 / 2; 
+		float value2 = 191 / 61;
+		double value3 = 60d / 6d;
+		float value4 = 5/2.5f; //float division
+		System.out.println("value 1=" + value1); 
+		System.out.println("value 2=" + value2);
+		System.out.println("value 3=" + value3);
+		System.out.println("value 4=" + value4);
+		
+		
+		int marker = 512;
+		double percentage = marker * 0.46f;
+		System.out.println("percentage :" + percentage);
+		
+
+	}
+
+}
