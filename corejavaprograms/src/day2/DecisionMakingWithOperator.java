804e5c51560de14913686c9658c2dcb1f0183d51
DELL <DELL@DESKTOP-MMSF2EA>
2025-09-27 10:59:31 +0530

    commit

---
+++ corejavaprograms/src/day2/DecisionMakingWithOperator.java
@@ -0,0 +1,14 @@
+package day2;
+
+public class DecisionMakingWithOperator {
+
+	public static void main(String[] args) {
+		for (int k = 3; k < 15; k++)
+		{
+		if (k%2 != 0)
+		continue;
+		System.out.print(k + " ");
+		}
+	}
+
+}
